bc71fa9a890cfb28bce99d26de026a1c0fc94b82
beefcarhorn <113559659+beefcarhorn@users.noreply.github.com>
2024-09-10 15:21:44 -0400

    feat: added types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 export interface ContributorActivity {
     total: number;
     author: {
         login: string;
     }
-=======
-export interface Commit {
-    commit: {
-        message: string;
-        author: {
-            name: string;
-            date: string;
-        };
-    };
->>>>>>> 978aafc1219aab965874cd1b5c803abcc157f8d0
 }
 
 export interface Contributor {
     login: string;
     contributions: string;
-<<<<<<< HEAD
 }
 
 export interface Issue {
@@ -32,6 +20,3 @@ export interface IssueSearchResponse {
     total_count: number;
     items: Issue[];
 }
-=======
-}
->>>>>>> 978aafc1219aab965874cd1b5c803abcc157f8d0
