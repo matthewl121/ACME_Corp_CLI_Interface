746efabd9d6e84628ea61c6b7c74319c876c2620
Matthew Li <mli007@hawaiiantel.net>
2024-09-11 14:47:33 -0400

    initiated json and js files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 "use strict";
+/*
+    This file is an example logical flow from a URL to
+    fetching and parsing repo data and calculating some metrics
+*/
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,12 +13,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+        while (_) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
             if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
@@ -35,81 +39,98 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var axios = require("axios");
-var GitHubRepoCollaborators = /** @class */ (function () {
-    function GitHubRepoCollaborators(repoOwner, repoName, token) {
-        this.repoOwner = repoOwner;
-        this.repoName = repoName;
-        this.token = token;
-        this.apiUrl = "https://api.github.com/repos/".concat(repoOwner, "/").concat(repoName, "/collaborators");
-        this.headers = {
-            'Authorization': "token ".concat(this.token),
-            'Accept': 'application/vnd.github.v3+json'
-        };
-    }
-    GitHubRepoCollaborators.prototype.fetchCollaborators = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var response, error_1;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        _a.trys.push([0, 2, , 3]);
-                        return [4 /*yield*/, axios.get(this.apiUrl, { headers: this.headers })];
-                    case 1:
-                        response = _a.sent();
-                        return [2 /*return*/, response.data];
-                    case 2:
-                        error_1 = _a.sent();
-                        if (axios.isAxiosError(error_1)) {
-                            if (error_1.response) {
-                                if (error_1.response.status === 404) {
-                                    console.error('Repository not found.');
-                                }
-                                else if (error_1.response.status === 403) {
-                                    console.error('Forbidden: Check your authentication token.');
-                                }
-                                else {
-                                    console.error("Error: ".concat(error_1.response.status, " - ").concat(error_1.response.statusText));
-                                }
-                            }
-                            else {
-                                console.error('An error occurred:', error_1.message);
-                            }
-                        }
-                        return [2 /*return*/, []];
-                    case 3: return [2 /*return*/];
+exports.__esModule = true;
+require("dotenv/config");
+var GithubApi_1 = require("./api/GithubApi");
+var metricCalcs_1 = require("./metricCalcs");
+var utils_1 = require("./utils/utils");
+var urlHandler_1 = require("./utils/urlHandler");
+var npmApi_1 = require("./api/npmApi");
+var main = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var token, inputURL, hostname, repoURL, npmPackageName, npmResponse, repoDetails, owner, repo, contributorActivity, busFactor, totalOpenIssues, totalClosedIssues, correctness, recentPullRequests, responsiveness, licenses, license;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                token = process.env.GITHUB_TOKEN || "";
+                inputURL = "https://www.npmjs.com/package/ts-node";
+                hostname = (0, urlHandler_1.extractDomainFromUrl)(inputURL);
+                if (!hostname || (hostname !== "www.npmjs.com" && hostname !== "github.com")) {
+                    return [2 /*return*/];
                 }
-            });
-        });
-    };
-    GitHubRepoCollaborators.prototype.printCollaborators = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var collaborators;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, this.fetchCollaborators()];
-                    case 1:
-                        collaborators = _a.sent();
-                        if (collaborators.length > 0) {
-                            console.log("Collaborators of ".concat(this.repoOwner, "/").concat(this.repoName, ":"));
-                            collaborators.forEach(function (collaborator) {
-                                console.log("- ".concat(collaborator.login, " (").concat(collaborator.type, ")"));
-                            });
-                        }
-                        else {
-                            console.log('No collaborators found or error in fetching data.');
-                        }
-                        return [2 /*return*/];
+                repoURL = "";
+                if (!(hostname === "www.npmjs.com")) return [3 /*break*/, 2];
+                npmPackageName = (0, urlHandler_1.extractNpmPackageName)(inputURL);
+                if (!npmPackageName) {
+                    return [2 /*return*/];
                 }
-            });
-        });
-    };
-    return GitHubRepoCollaborators;
-}());
-// Example usage:
-var repoOwner = 'octocat';
-var repoName = 'Hello-World';
-var token = 'your_github_token_here';
-var githubCollaborators = new GitHubRepoCollaborators(repoOwner, repoName, token);
-githubCollaborators.printCollaborators();
+                return [4 /*yield*/, (0, npmApi_1.fetchGithubUrlFromNpm)(npmPackageName)];
+            case 1:
+                npmResponse = _a.sent();
+                if (!(npmResponse === null || npmResponse === void 0 ? void 0 : npmResponse.data)) {
+                    return [2 /*return*/];
+                }
+                repoURL = npmResponse.data;
+                return [3 /*break*/, 3];
+            case 2:
+                // URL must be github, so use it directly
+                repoURL = inputURL;
+                _a.label = 3;
+            case 3:
+                repoDetails = (0, urlHandler_1.extractGithubOwnerAndRepo)(repoURL);
+                if (!repoDetails) {
+                    return [2 /*return*/];
+                }
+                owner = repoDetails[0], repo = repoDetails[1];
+                return [4 /*yield*/, (0, GithubApi_1.fetchContributorActivity)(owner, repo, token)];
+            case 4:
+                contributorActivity = _a.sent();
+                if (!(contributorActivity === null || contributorActivity === void 0 ? void 0 : contributorActivity.data)) {
+                    return [2 /*return*/];
+                }
+                return [4 /*yield*/, (0, utils_1.writeFile)(contributorActivity, "contributorActivity.json")];
+            case 5:
+                _a.sent();
+                busFactor = (0, metricCalcs_1.calcBusFactor)(contributorActivity.data);
+                return [4 /*yield*/, (0, GithubApi_1.fetchRecentIssuesByState)(owner, repo, "open", token)];
+            case 6:
+                totalOpenIssues = _a.sent();
+                return [4 /*yield*/, (0, GithubApi_1.fetchRecentIssuesByState)(owner, repo, "closed", token)];
+            case 7:
+                totalClosedIssues = _a.sent();
+                if (!(totalOpenIssues === null || totalOpenIssues === void 0 ? void 0 : totalOpenIssues.data) || !(totalClosedIssues === null || totalClosedIssues === void 0 ? void 0 : totalClosedIssues.data)) {
+                    return [2 /*return*/];
+                }
+                return [4 /*yield*/, (0, utils_1.writeFile)(totalOpenIssues, "totalOpenIssues.json")];
+            case 8:
+                _a.sent();
+                return [4 /*yield*/, (0, utils_1.writeFile)(totalClosedIssues, "totalClosedIssues.json")];
+            case 9:
+                _a.sent();
+                correctness = (0, metricCalcs_1.calcCorrectness)(totalOpenIssues.data.total_count, totalClosedIssues.data.total_count);
+                return [4 /*yield*/, (0, GithubApi_1.fetchRecentPullRequests)(owner, repo, token)];
+            case 10:
+                recentPullRequests = _a.sent();
+                if (!(recentPullRequests === null || recentPullRequests === void 0 ? void 0 : recentPullRequests.data)) {
+                    return [2 /*return*/];
+                }
+                return [4 /*yield*/, (0, utils_1.writeFile)(recentPullRequests, "recentPullRequests.json")];
+            case 11:
+                _a.sent();
+                responsiveness = (0, metricCalcs_1.calcResponsiveness)(totalClosedIssues.data.items, recentPullRequests.data.items);
+                return [4 /*yield*/, (0, GithubApi_1.fetchLicense)(owner, repo, token)];
+            case 12:
+                licenses = _a.sent();
+                if (!(licenses === null || licenses === void 0 ? void 0 : licenses.data)) {
+                    return [2 /*return*/];
+                }
+                return [4 /*yield*/, (0, utils_1.writeFile)(licenses, "licenses.json")];
+            case 13:
+                _a.sent();
+                license = licenses.data.license.name;
+                // Log the metrics
+                console.log("\n        --- METRICS --- \n        \n        Bus Factor:     ".concat(busFactor, " devs\n        Correctness:    ").concat(correctness, "%\n        Responsiveness: ").concat(responsiveness, " hours\n        License:        ").concat(license, "\n    "));
+                return [2 /*return*/];
+        }
+    });
+}); };
+main();
