07ee2384a1072ca90f8b8f73eb441f43e6bcc3d7
Matthew Li <mli007@hawaiiantel.net>
2024-09-18 22:36:15 -0400

    made changes to ./run install

--- src/run.js
+++ src/run.js
@@ -15,10 +15,33 @@ program
                 console.error(`%cError installing dependencies: ${stderr}`, `color: red`);
                 process.exit(1);
             }
-            console.log(`%c${stdout}`, 'color: green');
+            const addedPackages = stdout.match(/added (\d+) packages?/);
+            if (addedPackages && addedPackages[1]) {
+                const count = addedPackages[1];
+                console.log(`${count} dependencies were installed`);
+            } else {
+                console.log(`All dependencies are installed and up to date`);
+            }
+            // console.log(`%c${stdout}`, 'color: green');
         });
     });
 
+program
+    .argument('<file>', 'file to run')
+    .description('process URL of the file to run and output metrics in NDJSON format')
+    .action((file) => {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                console.error(`%cError reading file: ${err}`, `color: red`);
+                process.exit(1);
+            }
+            const urls = data.split('\n').filter(line => line.trim() !== '');
+            urls.forEach(url => {
+                const metrics = calculateMetrics(url);
+                console.log(JSON.stringify(metrics));
+            });
+        });
+    });
     //include the commands to run the tests etc
 
     program.parse(process.argv);
